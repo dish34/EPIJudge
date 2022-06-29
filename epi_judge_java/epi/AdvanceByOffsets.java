4d9179465b6029fb9849a2c0e4a8a75a8f2a4e82
Dishant Gandhi <dishantg@amdocs.com>
2022-06-29 23:08:49 +0530

    solved AdvanceByOffsets in O(n)

--- epi_judge_java/epi/AdvanceByOffsets.java
+++ epi_judge_java/epi/AdvanceByOffsets.java
@@ -7,7 +7,12 @@ public class AdvanceByOffsets {
   @EpiTest(testDataFile = "advance_by_offsets.tsv")
   public static boolean canReachEnd(List<Integer> maxAdvanceSteps) {
     // TODO - you fill in here.
-    return true;
+	  int futher_reach = 0;
+	  int end = maxAdvanceSteps.size() - 1;
+	  for(int i = 0; i <= futher_reach && futher_reach <= end; i++) {
+		  futher_reach = Math.max(futher_reach, maxAdvanceSteps.get(i) + i);
+	  }
+    return futher_reach >= end;
   }
 
   public static void main(String[] args) {

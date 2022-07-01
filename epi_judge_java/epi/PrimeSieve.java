aa1faabe7e2ba4bb69915e79b3a7bfaf8ffbcfac
Dishant Gandhi <dishantg@amdocs.com>
2022-07-01 11:40:44 +0530

    generatePrimes in o(n)

--- epi_judge_java/epi/PrimeSieve.java
+++ epi_judge_java/epi/PrimeSieve.java
@@ -2,13 +2,29 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 public class PrimeSieve {
   @EpiTest(testDataFile = "prime_sieve.tsv")
   // Given n, return all primes up to and including n.
   public static List<Integer> generatePrimes(int n) {
     // TODO - you fill in here.
-    return null;
+	List<Integer> prime = new ArrayList<>();
+	Boolean[] isPrime = new Boolean[n + 1];
+	Arrays.fill(isPrime, Boolean.TRUE);
+	isPrime[0] = false;
+	isPrime[1] = false;
+	for (int i = 2; i <= n; i++) {
+		if (isPrime[i]) {
+			prime.add(i);
+			for (int j = i; j <= n; j += i) {
+				isPrime[j] = false;
+			}
+		}
+	}
+
+	return prime;
   }
 
   public static void main(String[] args) {

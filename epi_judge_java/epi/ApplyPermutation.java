642b17422bf50806cf60387a1eac262835f8b56b
Dishant Gandhi <dishantg@amdocs.com>
2022-07-04 23:51:03 +0530

    Solved apply permutation in O(n)

--- epi_judge_java/epi/ApplyPermutation.java
+++ epi_judge_java/epi/ApplyPermutation.java
@@ -2,10 +2,21 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Collections;
 import java.util.List;
 public class ApplyPermutation {
   public static void applyPermutation(List<Integer> perm, List<Integer> A) {
     // TODO - you fill in here.
+	for (int i = 0; i < A.size(); i++) {
+		int next = i;
+		if (perm.get(next) >= 0)
+		{
+			Collections.swap(A, i, perm.get(next));
+			int temp = perm.get(next);
+			perm.set(next,  perm.get(next) - perm.size());
+			next = temp;
+		}
+	}
     return;
   }
   @EpiTest(testDataFile = "apply_permutation.tsv")
